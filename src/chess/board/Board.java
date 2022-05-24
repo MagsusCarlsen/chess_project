7c81eeac0999576889219e103d123f0a7df54dcf
Noah Osterholz <57216911+Noosterholz@users.noreply.github.com>
2022-05-24 12:29:06 +0200

    minor fix

--- src/chess/board/Board.java
+++ src/chess/board/Board.java
@@ -76,5 +76,11 @@ public class Board {
                 }
             }
         }
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                System.out.println(board[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
